b56f0d97395e3e028f45c94ef17dd9b903805ab0
azhuge233 <17763056+azhuge233@users.noreply.github.com>
2024-06-27 08:19:05 +0800

    Update notification string.

--- EGSFreeGamesNotifier/Strings/NotifyFormatStrings.cs
+++ EGSFreeGamesNotifier/Strings/NotifyFormatStrings.cs
@@ -5,114 +5,114 @@
 			"<b>New EGS Free Game</b>\n\n" +
 			"<b>{0}</b>\n\n" + // Title
 			"<i>{1}</i>\n\n" + // Description
-			"EGS 链接: <a href=\"{2}\" >{0}</a>\n" + // Url
-			"开始时间: {3}\n" + // StartTime
-			"结束时间: {4}\n\n" + // EndTime
+			"EGS Link: <a href=\"{2}\" >{0}</a>\n" + // Url
+			"Start Time: {3}\n" + // StartTime
+			"End Time: {4}\n\n" + // EndTime
 			"#EGS #{5}",
 
 			"<b>Upcoming EGS Free Game</b>\n\n" +
 			"<b>{0}</b>\n\n" + // Title
 			"<i>{1}</i>\n\n" + // Description
-			"EGS 链接: <a href=\"{2}\" >{0}</a>\n" + // Url
-			"开始时间: {3}\n" + // StartTime
-			"结束时间: {4}\n\n" + // EndTime
+			"EGS Link: <a href=\"{2}\" >{0}</a>\n" + // Url
+			"Start Time: {3}\n" + // StartTime
+			"End Time: {4}\n\n" + // EndTime
 			"#EGS #Upcoming #{5}",
 		};
 
 		internal static readonly List<string> barkPushFormat = new() {
 			"New EGS Free Game\n\n" +
 			"{0}\n\n" +
-			"EGS 链接: {1}\n" +
-			"开始时间: {2}\n" +
-			"结束时间: {3}\n",
+			"EGS Link: {1}\n" +
+			"Start Time: {2}\n" +
+			"End Time: {3}\n",
 
 			"Upcoming EGS Free Game\n\n" +
 			"{0}\n\n" +
-			"EGS 链接: {1}\n" +
-			"开始时间: {2}\n" +
-			"结束时间: {3}\n"
+			"EGS Link: {1}\n" +
+			"Start Time: {2}\n" +
+			"End Time: {3}\n"
 		};
 
 		internal static readonly List<string> emailPushHtmlFormat = new() {
 			"<p><b>{0}</b><br>" +
-			"EGS 链接: <a href=\"{1}\" > {0}</a><br><br>" +
-			"开始时间: {2}<br>" +
-			"结束时间: {3}<br>",
+			"EGS Link: <a href=\"{1}\" > {0}</a><br><br>" +
+			"Start Time: {2}<br>" +
+			"End Time: {3}<br>",
 
 			"<b><i>Upcoming</i></b>" +
 			"<p><b>{0}</b><br>" +
-			"EGS 链接: <a href=\"{1}\" > {0}</a><br><br>" +
-			"开始时间: {2}<br>" +
-			"结束时间: {3}<br>"
+			"EGS Link: <a href=\"{1}\" > {0}</a><br><br>" +
+			"Start Time: {2}<br>" +
+			"End Time: {3}<br>"
 		};
 
 		internal static readonly List<string> qqPushFormat = new() {
 			"New EGS Free Game\n\n" +
 			"{0}\n\n" +
-			"EGS 链接: {1}\n" +
-			"开始时间: {2}\n" +
-			"结束时间: {3}\n",
+			"EGS Link: {1}\n" +
+			"Start Time: {2}\n" +
+			"End Time: {3}\n",
 
 			"Upcoming EGS Free Game\n\n" +
 			"{0}\n\n" +
-			"EGS 链接: {1}\n" +
-			"开始时间: {2}\n" +
-			"结束时间: {3}\n"
+			"EGS Link: {1}\n" +
+			"Start Time: {2}\n" +
+			"End Time: {3}\n"
 		};
 
 		internal static readonly List<string> pushPlusPushHtmlFormat = new() {
 			"<p><b>{0}</b><br>" +
-			"EGS 链接: <a href=\"{1}\" > {0}</a><br><br>" +
-			"开始时间: {2}<br>" +
-			"结束时间: {3}<br>",
+			"EGS Link: <a href=\"{1}\" > {0}</a><br><br>" +
+			"Start Time: {2}<br>" +
+			"End Time: {3}<br>",
 
 			"<b><i>Upcoming</i></b>" +
 			"<p><b>{0}</b><br>" +
-			"EGS 链接: <a href=\"{1}\" > {0}</a><br><br>" +
-			"开始时间: {2}<br>" +
-			"结束时间: {3}<br>"
+			"EGS Link: <a href=\"{1}\" > {0}</a><br><br>" +
+			"Start Time: {2}<br>" +
+			"End Time: {3}<br>"
 		};
 
 		internal static readonly List<string> dingTalkPushFormat = new() {
 			"New EGS Free Game\n\n" +
 			"{0}\n\n" +
-			"EGS 链接: {1}\n" +
-			"开始时间: {2}\n" +
-			"结束时间: {3}\n",
+			"EGS Link: {1}\n" +
+			"Start Time: {2}\n" +
+			"End Time: {3}\n",
 
 			"Upcoming EGS Free Game\n\n" +
 			"{0}\n\n" +
-			"EGS 链接: {1}\n" +
-			"开始时间: {2}\n" +
-			"结束时间: {3}\n"
+			"EGS Link: {1}\n" +
+			"Start Time: {2}\n" +
+			"End Time: {3}\n"
 		};
 
 		internal static readonly List<string> pushDeerPushFormat = new() {
 			"New EGS Free Game\n\n" +
 			"{0}\n\n" +
-			"EGS 链接: {1}\n\n" +
-			"开始时间: {2}\n\n" +
-			"结束时间: {3}\n\n",
+			"EGS Link: {1}\n\n" +
+			"Start Time: {2}\n\n" +
+			"End Time: {3}\n\n",
 
 			"Upcoming EGS Free Game\n\n" +
 			"{0}\n\n" +
-			"EGS 链接: {1}\n\n" +
-			"开始时间: {2}\n\n" +
-			"结束时间: {3}\n\n"
+			"EGS Link: {1}\n\n" +
+			"Start Time: {2}\n\n" +
+			"End Time: {3}\n\n"
 		};
 
 		internal static readonly List<string> discordPushFormat = new() {
 			"New EGS Free Game\n\n" +
 			"{0}\n\n" +
-			"EGS 链接: {1}\n" +
-			"开始时间: {2}\n" +
-			"结束时间: {3}\n",
+			"EGS Link: {1}\n" +
+			"Start Time: {2}\n" +
+			"End Time: {3}\n",
 
 			"Upcoming EGS Free Game\n\n" +
 			"{0}\n\n" +
-			"EGS 链接: {1}\n" +
-			"开始时间: {2}\n" +
-			"结束时间: {3}\n"
+			"EGS Link: {1}\n" +
+			"Start Time: {2}\n" +
+			"End Time: {3}\n"
 		};
 		#endregion
 
