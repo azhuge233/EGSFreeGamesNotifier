bccd19df888243ce885350dd5e7af0dd5f488984
azhuge233 <17763056+azhuge233@users.noreply.github.com>
2026-02-04 08:35:02 +0800

    Use playwright to scrape GraphQL data.

--- EGSFreeGamesNotifier/Services/Scraper.cs
+++ EGSFreeGamesNotifier/Services/Scraper.cs
@@ -1,11 +1,18 @@
-﻿using Microsoft.Extensions.Logging;
-using EGSFreeGamesNotifier.Strings;
+﻿using EGSFreeGamesNotifier.Strings;
+using Microsoft.Extensions.Logging;
+using Microsoft.Playwright;
 
 namespace EGSFreeGamesNotifier.Services {
-	internal class Scraper(ILogger<Scraper> logger) : IDisposable {
-		private readonly ILogger<Scraper> _logger = logger;
+	internal class Scraper : IDisposable {
+		private readonly ILogger<Scraper> _logger;
 
 		internal HttpClient Client { get; set; } = new HttpClient();
+
+		public Scraper(ILogger<Scraper> logger) { 
+			_logger = logger;
+
+			Microsoft.Playwright.Program.Main(["install", "firefox"]);
+		}
 
 		internal async Task<Tuple<string, string>> GetSource() {
 			try {
@@ -15,8 +22,10 @@ namespace EGSFreeGamesNotifier.Services {
 
 				_logger.LogDebug(ScrapeStrings.debugGetGraphQLSource);
 				var graphQLUrl = $"{ScrapeStrings.EGSGraphQLUrl}?{ScrapeStrings.EGSGraphQLQueryParam}";
-				var graphQLSource =  await GetSourceWithHttpClient(graphQLUrl);
+				var graphQLSource =  await GetSourceWithPlaywright(graphQLUrl);
 				_logger.LogDebug($"Done: {ScrapeStrings.debugGetGraphQLSource}");
+
+				File.WriteAllText("debug_grapql.json", graphQLSource);
 
 				return new (weeklyFreeGameSource, graphQLSource);
 			} catch (Exception) {
@@ -29,22 +38,57 @@ namespace EGSFreeGamesNotifier.Services {
 
 		private async Task<string> GetSourceWithHttpClient(string url) {
 			try {
-				_logger.LogDebug(ScrapeStrings.debugGetSourceWithUrl, url);
+				_logger.LogDebug(ScrapeStrings.debugGetSourceWithHttpClient, url);
 
 				var response = await Client.GetAsync(url);
 				var result = await response.Content.ReadAsStringAsync();
 
-				_logger.LogDebug($"Done: {ScrapeStrings.debugGetSourceWithUrl}", url);
+				_logger.LogDebug($"Done: {ScrapeStrings.debugGetSourceWithHttpClient}", url);
 				return result;
 			} catch (Exception) {
-				_logger.LogError($"Error: {ScrapeStrings.debugGetSourceWithUrl}", url);
+				_logger.LogError($"Error: {ScrapeStrings.debugGetSourceWithHttpClient}", url);
 				throw;
-			} finally {
-				Dispose();
 			}
 		}
 
-		public void Dispose() { 
+		private async Task<string> GetSourceWithPlaywright(string url) {
+			try {
+				_logger.LogDebug(ScrapeStrings.debugGetSourceWithPlaywright, url);
+				var page = await GetNewPageInstance();
+
+				await page.GotoAsync(url, new PageGotoOptions { WaitUntil = WaitUntilState.NetworkIdle });
+
+				var source = await page.Locator("body > pre").InnerTextAsync();
+
+				await page.CloseAsync();
+
+				_logger.LogDebug($"Done: {ScrapeStrings.debugGetSourceWithPlaywright}", url);
+				return source;
+			} catch (Exception) {
+				_logger.LogError($"Error: {ScrapeStrings.debugGetSourceWithPlaywright}", url);
+				throw;
+			}
+		}
+
+		private static async Task<IPage> GetNewPageInstance() {
+			var playwright = await Playwright.CreateAsync();
+			var browser = await playwright.Firefox.LaunchAsync(new() { Headless = true });
+			var context = await browser.NewContextAsync();
+
+			var page = await context.NewPageAsync();
+			page.SetDefaultTimeout(10000);
+			page.SetDefaultNavigationTimeout(10000);
+			await page.RouteAsync("**/*", async route => {
+				var blockList = new List<string> { "stylesheet", "image", "font" };
+				if (blockList.Contains(route.Request.ResourceType)) await route.AbortAsync();
+				else await route.ContinueAsync();
+			});
+
+			return page;
+		}
+
+		public void Dispose() {
+			Client?.Dispose();
 			GC.SuppressFinalize(this);
 		}
 	}
--- EGSFreeGamesNotifier/Strings/ScrapeStrings.cs
+++ EGSFreeGamesNotifier/Strings/ScrapeStrings.cs
@@ -1,7 +1,7 @@
 ﻿namespace EGSFreeGamesNotifier.Strings {
 	internal class ScrapeStrings {
 		internal static readonly string EGSUrl = "https://store-site-backend-static.ak.epicgames.com/freeGamesPromotions";
-		internal static readonly string EGSGraphQLUrl = "https://store.epicgames.com/graphql?";
+		internal static readonly string EGSGraphQLUrl = "https://store.epicgames.com/graphql";
 
 		internal static readonly string EGSGraphQLQueryParam = @"operationName=searchStoreQuery&variables={""allowCountries"":""US"",""category"":""games/edition/base"",""comingSoon"":false,""count"":40,""country"":""US"",""freeGame"":true,""locale"":""en-US"",""sortBy"":""releaseDate"",""sortDir"":""DESC"",""start"":0,""onSale"":true,""withPrice"":false,""withPromotions"":true}&extensions={""persistedQuery"":{""version"":1,""sha256Hash"":""29d49ab31d438cd90be2d554d2d54704951e4223a8fcd290fcf68308841a1979""}}";
 
@@ -9,7 +9,8 @@
 		internal static readonly string debugGetSource = "Get source";
 		internal static readonly string debugGetWeeklyFreeGameSource = "Get weekly free game source";
 		internal static readonly string debugGetGraphQLSource = "Get GraphQL source";
-		internal static readonly string debugGetSourceWithUrl = "Getting source: {0}";
+		internal static readonly string debugGetSourceWithHttpClient = "Getting source with HttpClient: {0}";
+		internal static readonly string debugGetSourceWithPlaywright = "Getting source with Playwright: {0}";
 		#endregion
 	}
 }
